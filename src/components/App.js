d776628840fcf02d82ba95c737f47a2f80158f0a
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-25 15:16:04 +0200

    chore: add Root component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 
+import Root from './Root'
 import { Button } from './atoms/Button/Button'
 
 const App = () => (
-  <div>
+  <Root>
     <Button>Button</Button>
-  </div>
+  </Root>
 )
 
 export default hot(module)(App)
