b7b325ab6d53ed8d5a59f7a954d99dd50bf73349
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-25 13:58:36 +0200

    fix: reference

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import Button from './components/atoms/Button'
+import { Button } from './components/atoms/Button'
 
 render(<Button />, document.getElementById('root'))
