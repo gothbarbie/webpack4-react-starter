94294575fb0c725b2364b51768db6217e95accfc
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-25 16:22:45 +0200

    fix: use fragments

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { hot } from 'react-hot-loader'
 
 import Root from './Root'
+import Theme from './Theme'
 import ScrollToTop from './hoc/ScrollToTop'
 import Loading from './atoms/Loading/Loading'
+import Normalize from './Normalize/Normalize'
 
 const Home = () => <div>Home</div>
 
@@ -13,22 +15,27 @@ const ShowButton = () => <Loading />
 const App = () => (
   <Root>
     <Router>
-      <ScrollToTop>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/loading">Loading</Link>
-              </li>
-            </ul>
-          </nav>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/loading" component={ShowButton} />
-        </div>
-      </ScrollToTop>
+      <Theme>
+        <>
+          <Normalize />
+          <ScrollToTop>
+            <>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/loading">Loading</Link>
+                  </li>
+                </ul>
+              </nav>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/loading" component={ShowButton} />
+            </>
+          </ScrollToTop>
+        </>
+      </Theme>
     </Router>
   </Root>
 )
