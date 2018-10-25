0a87f4116366b72006688d993556baa7749982e6
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-25 13:56:07 +0200

    test: add basic snapshot

---
+++ src/components/atoms/Button/__tests__/Button.spec.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import { Button } from '../Button'
+
+describe('components/atoms/Button', () => {
+  it('renders', () => {
+    const component = shallow(<Button>Test</Button>)
+
+    expect(component).toMatchSnapshot()
+  })
+})
