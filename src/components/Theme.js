71217460c78f8859125d937a2d850b3f6092bc58
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-25 16:22:36 +0200

    feat: add ThemeProvider

---
+++ src/components/Theme.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+
+import theme from './themes/default'
+
+const Theme = ({ children }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+)
+
+export default Theme
