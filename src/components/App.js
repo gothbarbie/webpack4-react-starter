2d81213f8f5bec9a4878b947804018a969ce541c
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-25 15:25:35 +0200

    chore: add router

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,32 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { hot } from 'react-hot-loader'
 
 import Root from './Root'
 import { Button } from './atoms/Button/Button'
 
+const Home = () => <div>Home</div>
+
+const ShowButton = () => <Button>Button</Button>
+
 const App = () => (
   <Root>
-    <Button>Button</Button>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/button">Button</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/button" component={ShowButton} />
+      </div>
+    </Router>
   </Root>
 )
 
