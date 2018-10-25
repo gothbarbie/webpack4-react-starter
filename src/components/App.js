d22019db443d4ce48ff89496ce906e49c75f9df1
Hanna Söderström <gothbarbie84@gmail.com>
2018-10-25 15:33:11 +0200

    feat: scroll to top on Link click

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { hot } from 'react-hot-loader'
 
 import Root from './Root'
+import ScrollToTop from './hoc/ScrollToTop'
 import { Button } from './atoms/Button/Button'
 
 const Home = () => <div>Home</div>
@@ -12,20 +13,22 @@ const ShowButton = () => <Button>Button</Button>
 const App = () => (
   <Root>
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/button">Button</Link>
-            </li>
-          </ul>
-        </nav>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/button" component={ShowButton} />
-      </div>
+      <ScrollToTop>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/button">Button</Link>
+              </li>
+            </ul>
+          </nav>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/button" component={ShowButton} />
+        </div>
+      </ScrollToTop>
     </Router>
   </Root>
 )
